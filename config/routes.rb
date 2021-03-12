69cc8286bf322e733f794fc5ca565a0a2bdf64a8
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-03-12 10:27:52 -0300

    feat: make new router/method/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
+
   get 'welcome/index'
+  get '/inicio', to: 'welcome#inicio'
+
   resources :coins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
