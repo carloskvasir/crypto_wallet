29abbc66cf2b4a3392b61f7ffc7599a3ae282e94
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-03-15 16:01:16 -0300

    feat: style output of seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,26 @@
-puts 'Cadastrando as moedas...'
-Coin.create!(
-  description: 'Bitcoin',
-  acronym: 'BTC',
-  url_image: 'http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png'
-)
+spinner = TTY::Spinner.new(':spinner  Executando seeds', format: :dots)
+spinner.auto_spin
+coins =
+  [
+    {
+      description: 'Bitcoin',
+      acronym: 'BTC',
+      url_image: 'http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png'
+    },
+    {
+      description: 'Ethereum',
+      acronym: 'ETH',
+      url_image: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Ethereum_logo_2014.svg'
+    },
+    {
+      description: 'Dash',
+      acronym: 'DASH',
+      url_image: 'https://ih1.redbubble.net/image.565893629.7574/st,small,845x845-pad,1000x1000,f8f8f8.u2.jpg'
+    }
+  ]
 
-Coin.create!(
-  description: 'Ethereum',
-  acronym: 'ETH',
-  url_image: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Ethereum_logo_2014.svg'
-)
+coins.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
 
-Coin.create!(
-  description: 'Dash',
-  acronym: 'DASH',
-  url_image: 'https://ih1.redbubble.net/image.565893629.7574/st,small,845x845-pad,1000x1000,f8f8f8.u2.jpg'
-)
-
-puts 'Moedas cadastradas com sucesso!'
+spinner.success('(concluído)')
