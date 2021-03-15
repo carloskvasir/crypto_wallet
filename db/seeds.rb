ca12cf953ee5cbb65c0c868b9ff730cb57df632b
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-03-15 14:07:53 -0300

    feat: using basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cadastrando as moedas...'
+Coin.create!(
+  description: 'Bitcoin',
+  acronym: 'BTC',
+  url_image: 'http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png'
+)
+
+Coin.create!(
+  description: 'Ethereum',
+  acronym: 'ETH',
+  url_image: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Ethereum_logo_2014.svg'
+)
+
+Coin.create!(
+  description: 'Dash',
+  acronym: 'DASH',
+  url_image: 'https://ih1.redbubble.net/image.565893629.7574/st,small,845x845-pad,1000x1000,f8f8f8.u2.jpg'
+)
+
+puts 'Moedas cadastradas com sucesso!'
