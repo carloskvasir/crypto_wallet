0ab3f16606c78eaff129272093e9a5a5e4d78f88
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-03-15 12:01:19 -0300

    fix: style warnings

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -57,13 +57,14 @@ class CoinsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_coin
-      @coin = Coin.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def coin_params
-      params.require(:coin).permit(:description, :acronym, :url_image)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_coin
+    @coin = Coin.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def coin_params
+    params.require(:coin).permit(:description, :acronym, :url_image)
+  end
 end
