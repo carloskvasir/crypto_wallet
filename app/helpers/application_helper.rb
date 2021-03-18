ed5ab6cb05507ca869a9cb3fbd8d9c0e772ff465
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-03-18 11:15:13 -0300

    feat: using I18n to show atual locale

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+
+  # Get natural text based in +atual_locale+
+  def locale(atual_locale)
+    I18n.locale == :en ?
+      'Estados Unidos' :
+      'Português do Brasil'
+  end
+
   def date_brazil(data_us)
     data_us.strftime('%d/%m/%Y')
   end
