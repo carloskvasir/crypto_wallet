5694af452b6b2c96cf77c725c1d6956d728cdd03
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-03-19 10:37:59 -0300

    feat: Using cookie and session

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    cookies[:curso] = "Curso de Ruby =)[🍪 - Cookie]"
+    session[:curso] = "Curso de Ruby =)[⚙️  - session]"
+  end
 
   def inicio; end
 end
